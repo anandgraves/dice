455efa62274aaadf94070f47d8ec156f6fe6c7de
Anand Graves <anandgraves@users.noreply.github.com>
2020-03-04 17:03:12 +0100

    added dice project

---
+++ index.js
@@ -0,0 +1,45 @@
+(function () {
+'use strict';
+
+async function init() {
+  try {
+    const response = await fetch('./wordslist.json')
+    const wordslist = await response.json()
+    const button = document.querySelector('[data-generate]');
+    const input = document.querySelector('[data-passphrase]');
+
+    button.onclick = function () {
+      const passphrase = diceThrows().map(dice => wordslist[dice])
+      input.textContent = passphrase.join(' ');
+    }
+  } catch(error) {
+    console.error(error)
+  }
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getDiceNumber() {
+	var MINIMUM = 1;
+	var MAXIMUM = 6;
+	return getRandomInt(MINIMUM, MAXIMUM)
+}
+
+function createDiceThrows() {
+  return Array.from({ length: 12 },
+    () => Array.from({ length: 5 }, () => getDiceNumber())
+  )
+}
+
+function joinDiceThrows(array) {
+	return array.map(value => Number(value.join('')))
+}
+
+function diceThrows() {
+	return joinDiceThrows(createDiceThrows())
+}
+
+init();
+}());
